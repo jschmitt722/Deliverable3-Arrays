d37e80f3fd6b9aabe108aa87873b1388e7474a3e
jds07 <jds07@LAPTOP-F33C9EO1>
2020-02-06 18:04:00 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+    Author: Jesse Schmitt
+    Date: 2/5/2020
+    Comments: This is my C# console program to demonstrate arrays
+  
+ */
+
+using System;
+
+namespace Deliverable3_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int SIZE = 25; // Identifies size of the array
+            int number = 1; // Begins Array at the value of 1
+            int[] numbersArray = new int[SIZE]; // creats array
+
+            // Buils loop for array
+            for (int index = 0; index < SIZE; index++)
+            {
+                numbersArray[index] = number;  // Sets value for this index in array
+               // displays "Element Value" and current vale of array
+                Console.WriteLine("Element value = " + numbersArray[index].ToString()); 
+                number++; // Inceases value by 1
+
+            }
+        }
+    }
+}
